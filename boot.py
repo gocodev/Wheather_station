113ffbe9a32e3bf3860ea5cefe3835e9e6982e38
Gocodev <gocoplus@gmail.com>
2020-02-03 00:00:22 +0100

    Add WiFiManager feature from Tayfunulu
    Signed-off-by: Gocodev <gocoplus@gmail.com>

--- boot.py
+++ boot.py
@@ -1,10 +1,11 @@
 # Complete project details at https://RandomNerdTutorials.com
+import wifimgr
 
 try:
   import usocket as socket
 except:
   import socket
-  
+
 from time import sleep
 
 from machine import Pin, I2C
@@ -21,6 +22,17 @@ import BME280
 # ESP8266 - Pin assignment
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
 
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
+
+
+# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
+print("ESP OK")
+
+"""
 ssid = 'NETIASPOT-2.4GHz-9MAN'
 password = 'PbkT3e8d'
 
@@ -32,8 +44,10 @@ station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(ssid, password)
 
+
 while station.isconnected() == False:
   pass
-  
+
 print('Connection successful')
 print(station.ifconfig())
+"""
